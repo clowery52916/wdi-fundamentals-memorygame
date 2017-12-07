90ae348467b772db438903e1d87b4a9d520fe16d
[Courtney Lowery] <clowery52916@users.noreply.github.com>
2017-12-07 16:05:57 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 //create cards in game
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo = cards[3];
-cardsInPlay.push = (cardTwo);
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2) {
-	console.log("You have selected 2 cards.");
+var checkForMatch = function() {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert ("You found a match!");
+} 	else { 
+		alert ("Sorry, try again.");
 }
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert ("You found a match!");
-} else {alert ("Sorry, try again.");
-}
+};
 
+var flipCard = function(cardId) {
+	//user flips card and we can tell what card is picked 
+console.log("User flipped " + cards[cardId]);
+	if (cardsInPlay.length === 2) {
+	//do something?
+}
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
