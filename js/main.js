087a59d45a236025fbbb31775a2bbce59201e463
[Courtney Lowery] <clowery52916@users.noreply.github.com>
2017-12-14 10:34:55 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 //create cards in game
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank:'queen',
+suit: 'hearts',
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank:'queen',
+suit: 'diamonds',
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: 'king',
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+];
+//card properties
+
 var cardsInPlay = [];
 var checkForMatch = function() {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -11,13 +34,20 @@ var checkForMatch = function() {
 
 var flipCard = function(cardId) {
 	//user flips card and we can tell what card is picked 
-console.log("User flipped " + cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+//add queen and king cards to array
+cardsInPlay.push(cards[cardId].rank);
+//display
+console.log (cards[cardId].cardImage);
+console.log (cards[cardId].suit);
+
 	if (cardsInPlay.length === 2) {
 	//do something?
 }
-cardsInPlay.push(cards[cardId]);
+
 checkForMatch();
 };
 
 flipCard(0);
 flipCard(2);
+//card methods
